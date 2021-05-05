322dd56b518bc0760748b1f8eb32712c4a84fa24
kmayer <kylemayer@x.local>
2021-05-04 17:00:33 -0700

    added results divs

--- app.js
+++ app.js
@@ -2,16 +2,18 @@
 const button = document.getElementById('guess-button');
 const userInput = document.getElementById('user-input');
 const resultsDiv = document.getElementById('results');
+const highLow = document.getElementById('high-low');
+const attempts = document.getElementById('attempts');
 
 
 // initialize state
 button.addEventListener('click', () => {
-    
+    console.log(button, userInput, resultsDiv, highLow, attempts);
 
 });
 
 
-
+// Math.ceil(Math.random() * 20)
 
 
 // set event listeners to update state and DOM
